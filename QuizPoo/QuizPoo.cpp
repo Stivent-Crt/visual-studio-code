052973cdca5f5e8b6751cbc37ff43c8e1e5d6794
Stivent_crt <andrescortesg30@gmail.com>
2026-05-06 11:02:54 -0500

    ya se hicieron los comandos basicos de la interfaz

--- QuizPoo/QuizPoo.cpp
+++ QuizPoo/QuizPoo.cpp
@@ -45,5 +45,56 @@ public:
             std::cout << "Producto no reconocido" << std::endl;
             precio = 0.0;
         }
+        std::cout << "El precio total del producto es: " << precio << std::endl;
+    }
+    class mecato {
+    //atributos
+    public:
+        int cantidad; //cantidad de producto en unidades
+        std::string producto; //papas, galletas, chocolates, etc.
+        double precio=0.0; //precio del producto
+    //metodos
+    void calcularPrecio(){
+        if (producto == "chocorramo") {
+            precio = cantidad * 3000; //precio por caja de leche
+        } else if (producto == "gansito") {
+            precio = cantidad * 2500; //precio por unidad de huevo
+        } else if (producto == "detodito") {
+            precio = cantidad * 6000; //precio por 1 barra de 500 gramos de queso
+        } else if (producto == "galletas festival") {
+            precio = cantidad * 1200; //precio por 1 paquete de 600 gramos de queso crema
+        } else if (producto == "nutella") {
+            precio = cantidad * 18000; //precio por la caja de 750 gramos de mantequilla
+        } else if (producto == "postre yogurt") {
+            precio = cantidad * 4500; //precio por 1 litro de yogurt
+        } else {
+            std::cout << "Producto no reconocido" << std::endl;
+            precio = 0.0;
+        }
+        std::cout << "El precio total del producto es: " << precio << std::endl;
     }
 };
+int main(){
+    std::cout << "Ingrese el producto lacteo que desea comprar: ";
+    std::string productoLacteo;
+    std::cin >> productoLacteo;
+    std::cout << "Ingrese la cantidad del producto lacteo que desea comprar: ";
+    int cantidadLacteo; 
+    std::cin >> cantidadLacteo;
+    Lacteos lacteo;
+    lacteo.setProducto(productoLacteo);
+    lacteo.setCantidad(cantidadLacteo);
+    lacteo.calcularPrecio();
+    std::cout << "Ingrese el producto de mecato que desea comprar: ";
+    std::string productoMecato;
+    std::cin >> productoMecato;
+    std::cout << "Ingrese la cantidad del producto de mecato que desea comprar: ";
+    int cantidadMecato;
+    std::cin >> cantidadMecato;
+    mecato mecatito;
+    mecatito.producto = productoMecato;
+    mecatito.cantidad = cantidadMecato;
+    mecatito.calcularPrecio();
+
+    return 0;
+}
